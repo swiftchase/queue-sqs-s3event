72f49b610afc303c84ef7e72db7d5a63ef89a924
Marcus Welz <marcus@swiftchase.com>
2015-04-05 00:43:23 -0400

    removed unnecessary config.

--- Module.php
+++ Module.php
@@ -7,7 +7,7 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return [];
     }
 
     public function getAutoloaderConfig()
